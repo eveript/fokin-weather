0c13f301bf2ca1be2d664fae34e3faad4bfd88b2
jinha <gojinha@naver.com>
2020-10-27 03:20:03 +0900

    로케이션 추가, 로딩페이지 추가, useGeoLocation 훅

--- App.js
+++ App.js
@@ -1,21 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import * as Location from "expo-location";
 
+import Loading from "./Loading";
+
+const useGeoLocation = () => {
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  const initLocation = async () => {
+    try {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== 'granted') {
+        setError("Permission to access location was denied")
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  useEffect(() => {
+    initLocation()
+  }, [])
+
+  return {
+    location,
+    error,
+  }
+}
 export default function App() {
+  const { location } = useGeoLocation()
+  console.log(location)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Loading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
